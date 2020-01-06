da375570868e3beb396a13982a0713f3cfebe0de
poti <poti@potinoMacBook-Pro.local>
2020-01-06 18:12:42 +0900

    flutterで画像の出力が可能に！画像を保存するためにがgem的なものがいるのかな？

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "flutter demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: "わltyしょう"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({ Key key, this.title }) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() =>  _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.network("https://nzigen.com/flutter-reference/assets/img/samples/logo-jumper.png"),
+          Image.asset("images/riku.jpg"),
+        ],
+      ),
+    );
+  }
+}
